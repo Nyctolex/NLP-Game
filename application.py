26edc37674045bfcdde53bfea01698880090b835
Alex Chen <nyctolex@gmail.com>
2023-05-30 14:48:02 +0300

    adding NLP routes

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify, send_from_directory
 import os
-
+from wordHandler import WordHandler
 application = Flask(__name__, static_folder='build')
 app = application
-# if not 'wordHandler' in globals():
-#     wordHandler = WordHandler()
+if not 'wordHandler' in globals():
+    wordHandler = WordHandler()
 
 # Serve React App
 @app.route('/', defaults={'path': ''})
@@ -17,17 +17,29 @@ def serve(path):
 
 @app.route("/cards", methods=['GET'])
 def cards():
-    c = []
-    for i in range(5*5):
-        c.append({"title": "Card"+str(i),
-                  "description": [f'Description of card {i}']})
-    # cards_dict = wordHandler.get_words(5*5)
-    return  {"cards": c}
+    cards_dict = wordHandler.get_words(5*5)
+    return  {"cards": cards_dict}
 
 @app.route("/newBoards", methods=['GET'])
 def newBoard():
     cardsdict = cards()
     return cardsdict
+
+@app.route('/association', methods=['POST'])
+def association():
+    word = request.json.get('word')
+    words = request.json.get('words')
+    num = int(request.json.get('num'))
+    closestWords = wordHandler.get_k_nearest_neighbors(word, words, num)
+    # print('Received word:', word)
+    # print('Received words:', words)
+    # print('Received num:', num)
+    # closestWords = random.sample(words, num)
+    if closestWords is None:
+        return "No similar words found", 400
+    response = {'closetWords': closestWords}
+    return jsonify(response)
+
 
 
 if __name__ == "__main__":
